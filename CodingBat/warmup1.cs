7b4063fb261e49fa6f602d2642a3ff84dce9f5f4
pavithrarani <pavithra.be08@gmail.com>
2017-01-14 11:52:41 +0530

    Checkin warmup1 solutions<reDo> because project restructing

---
+++ CodingBat/warmup1.cs
@@ -0,0 +1,44 @@
+﻿namespace codingBat
+{
+    public class warmup1
+    {
+        public bool sleep(bool weekday, bool vacation)
+        {
+            if (!weekday && !vacation)
+                return true;
+            else if (weekday && !vacation)
+                return false;
+            else
+            {
+                return true;
+            }
+
+        }
+
+        //smile
+        public bool smile(bool aSmile, bool bSmile)
+        {
+            if (aSmile && bSmile)
+            {
+                return true;
+            }
+            else if (aSmile && !bSmile) return false;
+            else return true;
+        }
+        //sum
+        public int compute(int n1, int n2)
+        {
+
+            if (n1 != n2)
+            {
+                return n1 + n2;
+
+            }
+            else return 2 * (n1 + n2);
+        }
+        static void Main(string[] args)
+        {
+            System.Console.Write("hello");
+        }
+    }
+}
---
+++ CodingBatTest/warmup1Test.cs
@@ -0,0 +1,77 @@
+﻿using codingBat;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingBatTest
+{
+    [TestClass]
+    public class warmup1Test
+    {
+        //sleepIn
+        [TestMethod]
+        public void sleepInShouldReturnTrueIfNotWeekdayNotVacation()
+        {
+            warmup1 sp = new warmup1();
+            bool res = sp.sleep(false, false); // → true
+            Assert.AreEqual(res, true);
+        }
+        [TestMethod]
+        public void sleepInShouldReturnTrueIfWeekdayNotVacation()
+        {
+            warmup1 sp = new warmup1();
+            bool res = sp.sleep(true, false); // → false
+            Assert.AreEqual(res, false);
+        }
+
+        [TestMethod]
+        public void sleepInShouldReturnTrueIfNotWeekdayIfVacation()
+        {
+            warmup1 sp = new warmup1();
+            bool res = sp.sleep(false, true); // → true
+            Assert.AreEqual(res, true);
+        }
+        //----monkeyTrouble
+        [TestMethod]
+        public void inTroubleIfBothMonkeysSmiling()
+        {
+            //monkeyTrouble(true, true) → true
+            warmup1 mt = new warmup1();
+            bool res = mt.smile(true, true);
+            Assert.AreEqual(res, true);
+        }
+        [TestMethod]
+        public void inTroubleIfBothMonkeysAreNotSmiling()
+        {
+            //monkeyTrouble(false, false) → true
+            warmup1 mt = new warmup1();
+            bool res = mt.smile(false, false);
+            Assert.AreEqual(res, true);
+        }
+        [TestMethod]
+        public void noTroubleIfEitherOfMonkeysSmiling()
+        {
+            //monkeyTrouble(true, false) → false
+            warmup1 mt = new warmup1();
+            bool res = mt.smile(true, false);
+            Assert.AreEqual(res, false);
+        }
+        //--- sumTest
+        [TestMethod]
+        public void sumWhenNumberNotEqual()
+        {
+            int n1 = 10, n2 = 11;
+            warmup1 obj = new warmup1();
+            int res = obj.compute(n1, n2);
+            Assert.AreEqual(21, res);
+
+        }
+        [TestMethod]
+        public void sumWhenNumberEqual()
+        {
+            int n1 = 10, n2 = 10;
+            warmup1 obj = new warmup1();
+            int res = obj.compute(n1, n2);
+            Assert.AreEqual(40, res);
+
+        }
+    }
+}
